8e24903793ffe010f5097420a7abce4964f092b4
AlexandreTapadinhas <alexandre22tapadinhas@gmail.com>
2021-05-31 02:55:27 +0100

    trigger de comentarios no script comandos.sql

--- comandos.sql
+++ comandos.sql
@@ -80,6 +80,9 @@ INSERT INTO registolicitacao(preco_licitacao,data_licitacao,leilao_id_leilao,uti
 INSERT INTO registolicitacao(preco_licitacao,data_licitacao,leilao_id_leilao,utilizador_user_name) VALUES (156,'28-05-2021 06:02:15',123,'tapadinhas');
 INSERT INTO registolicitacao(preco_licitacao,data_licitacao,leilao_id_leilao,utilizador_user_name) VALUES (196,'29-05-2021 19:19:19',123,'ines');
 
+/*notificacao de licitacao ultrapassada*/
+DROP trigger if exists avisolicitacao on registolicitacao;
+DROP function if exists novalicitacao cascade;
 
 CREATE function novalicitacao() returns trigger
 language plpgsql
@@ -110,5 +113,39 @@ after insert or update or delete on registolicitacao
 for each row
 execute procedure novalicitacao();
 
-DROP trigger if exists avisolicitacao on registolicitacao;
-DROP function if exists novalicitacao cascade;
+/*notificacao de novo comentario*/
+DROP trigger if exists aviso_comentario on comentarios;
+DROP function if exists noti_novo_comentario cascade;
+
+
+CREATE function noti_novo_comentario() returns trigger
+language plpgsql
+AS $$
+DECLARE
+	noti_count numeric;
+    msg varchar;
+	
+	c1 cursor for
+		select utilizador_user_name, data_pub
+		from comentarios
+        where comentarios.leilao_id_leilao = new.leilao_id_leilao 
+		    and comentarios.utilizador_user_name != new.utilizador_user_name;
+		
+BEGIN
+	select max(id_noti) into noti_count from notificacao;
+    msg := 'Nova mensagem no mural no leilão ' || new.leilao_id_leilao;
+
+	for r in c1
+	loop
+        noti_count := noti_count + 1;
+        insert into notificacao(id_noti, msg, data, is_open, utilizador_user_name)
+		values(noti_count, msg, r.data_pub, false, new.utilizador_user_name);
+	end loop;
+	return new;
+end;
+$$;
+
+create trigger aviso_comentario
+after insert on comentarios
+for each row
+execute procedure noti_novo_comentario();
